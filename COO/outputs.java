995abc56c61b9d449b5caf7ad546e4d254424fb7
Fontanezi <fontanezieducational@gmail.com>
2024-03-14 09:44:15 -0300

    initialize JAVA

---
+++ COO/outputs.java
@@ -0,0 +1,14 @@
+class Outputs {
+    public static void main(String[] args) {
+        System.out.println("Usando println imprimimos uma linha. Cada uso pula uma linha.");
+        System.out.print("Usando somente print, também imprimimos, mas não pula linha.");
+        System.out.println("Podemos imprimir nùmeros e até operações matemáticas:");
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(1 + 4);
+
+        // Comentários JAVA
+        /* comentários em Java são idênticos 
+        aos comentários em C */
+    }
+}
---
+++ COO/strings.java
@@ -0,0 +1,11 @@
+class strings {
+    public static void main(String[] args) {
+        String greeting = "Hello!";
+        // Strings are objects, so they have their own methods!
+
+        System.out.println("The lenght of the string is: " + greeting.length());
+        System.out.println(greeting.toUpperCase()); 
+        System.out.println(greeting.toLowerCase()); 
+        System.out.println(greeting.indexOf("o"));
+    }
+}
