ff3bd501c138ff747163d4ab462014afbd448b94
Fontanezi <fontanezieducational@gmail.com>
2024-05-27 22:57:14 -0300

    DELETE

--- COO/forEach.java
+++
@@ -1,9 +0,0 @@
-class ForEach {
-    public static void main(String[] args) {
-        String[] cars = { "Volvo", "Ferrari", "Mercedes", "Porsche" };
-        for (String i : cars) {
-            System.out.println(i);
-        }
-        // Will read through the array, a new loop!!!
-    }
-}
